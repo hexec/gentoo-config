3be94ad4dddde2eb213c4a4ea07431c1238c017d
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2017-09-16 14:30:52 +0200

    bump

---
+++ chroot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mount --rbind /dev/ dev
+mount --make-rslave  dev
+mount --rbind /sys/ sys 
+mount --make-rslave sys
+mount -t proc /proc  proc
+mount --rbind /tmp tmp
+chroot . /bin/bash
+
