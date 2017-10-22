239fbe821c589eed10a81d499fc31c007424a1cf
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2017-10-22 20:03:26 +0200

    update gentoo config

--- chroot.sh
+++ chroot.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mount --rbind /dev/ dev
 mount --make-rslave  dev
-mount --rbind /sys/ sys 
+mount --rbind /sys/ sys
 mount --make-rslave sys
 mount -t proc /proc  proc
 mount --rbind /tmp tmp
 chroot . /bin/bash
+
 
