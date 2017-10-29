b39ad5ce0fec2f8f2cefaefc05ed983881adbb65
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2017-10-29 13:43:35 +0100

    update gentoo config

--- grub.sh
+++ grub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dosfsck dosfsck -w -r -l -a -v -t /dev/sda2
+dosfsck -w -r -l -a -v -t /dev/sda2
 fsck /dev/sda4
 grub-install --target=x86_64-efi --efi-directory=/boot /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
